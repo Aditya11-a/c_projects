b77b8e1b98607ae33d63073631fbf6d24bd06075
Aditya11-a <adityaraj5491@gmail.com>
2026-08-07 09:06:12 +0000

    changed first1.exe

--- first1.c
+++ first1.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-int j = 42;
-void fun3(){
-    int i =11, j=999;
-    printf("\t\t\tin [fun3] i @ 0x%016x and i=%d\n",&i,i);
-    printf("\t\t\tin [fun3] j @ 0x%016x and j=%d\n",&j,j);
+#include <string.h>
+void *errorchecked_malloc(unsigned int);
+int main(int argc, char* arg[]){
+char *char_ptr;
+int *int_ptr;
+int mem_size;
+if (argc <2){
+ mem_size = 50;
 }
-void fun2(){
-    int i=7;
-    printf("\t\tin [fun2] i @ 0x%016x and i=%d\n",&i,i); 
-    printf("\t\tin [fun2] j @ 0x%016x and j=%d\n",&j ,j);
-    printf("\t\tin [func2] setting j = 1337\n");
-    j=1337;
-    fun3();
-    printf("\t\tback in [fun2] i @ 0x%016x and i=%d\n",&i,i);
-    printf("\t\tback in [fun2] j @ 0x%016x and j=%d\n",&j,j);
+else{
+    mem_size= atoi(arg[1]);
 }
-void fun1(){
-    int i =5;
-    printf("\tin [fun1] i @ 0x%016x and i=%d\n",&i,i);
-    printf("\tin [fun1] j @ 0x%016x and j=%d\n",&j,j);
-    fun2();
-    printf("\tback in [fun1] i @ 0x%016x and i=%d\n",&i,i);
-    printf("\tback in [fun1] j @ 0x%016x and j=%d\n",&j,j);
+if (mem_size <36){
+    fprintf(stderr,"give the size bigger than 36");
+    exit(-1);
 }
-int main(){
-    int i=3;
-    printf("in [main] i @ 0x%016x and i=%d\n",&i,i);
-    printf("in [main] j @ 0x%016x and j=%d\n",&j,j);
-    fun1();
-    printf("back in [main] i @ 0x%016x and i=%d\n",&i,i);
-    printf("in [main] i @ 0x%016x and j=%d\n",&j,j);
+printf("allocating %d bytes in heap to char_ptr\n",mem_size);
+char_ptr = (char *)errorchecked_malloc(mem_size);
+
+strcpy(char_ptr,"This memory is located in the heap\n");
+printf("char_ptr %p --> %s\n",char_ptr,char_ptr);
+mem_size=12;
+printf("allocating %d bytes in heap to int_ptr\n",mem_size);
+int_ptr = (int*)errorchecked_malloc(mem_size);
+
+*int_ptr=1197;
+printf("int_ptr %p --> %d\n",int_ptr,*int_ptr);
+printf("freeing char_ptr\n");
+free(char_ptr);
+mem_size = 23;
+printf("allocating new %d bytes of memory\n",mem_size);
+char_ptr = (char*)errorchecked_malloc(mem_size);
+
+strcpy(char_ptr,"new memmory allocated\n");
+printf("char_ptr %p --> %s\n",char_ptr,char_ptr);
+printf("freeing char_ptr\n");
+free(char_ptr);
+printf("freeing int_ptr\n");
+free(int_ptr);
+return 0;
+}
+
+void *errorchecked_malloc(unsigned int size){
+void *ptr = malloc(size);
+if (ptr == NULL){
+    fprintf(stderr,"can't allocate memory");
+    exit(-1);
+}
+return ptr;
 }
