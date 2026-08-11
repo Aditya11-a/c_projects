84435bccd3f5a3a917b95671acfcde87f45e3279
Aditya11-a <adityaraj5491@gmail.com>
2026-08-11 16:02:33 +0000

    added error.h file

---
+++ error.h
@@ -0,0 +1,16 @@
+void fatal(char *message){
+    char error_message[100];
+
+    strcpy(error_message,"[!!] FATAL ERROR ");
+    strncat(error_message,message,83);
+    perror(error_message);
+    exit(EXIT_FAILURE);
+}
+
+void *ec_malloc(unsigned int size){
+    void *ptr;
+    ptr = malloc(size);
+    if (ptr == NULL)
+    fatal("error while allocating memmory in ec_malloc");
+    return ptr;
+}
