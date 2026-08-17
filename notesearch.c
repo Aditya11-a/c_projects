6d8b9aedbe3865d13b316adec9d108c26bffe141
Aditya11-a <adityaraj5491@gmail.com>
2026-08-17 15:32:17 +0000

    added search_filter function in notesearch.c with new logic

--- notesearch.c
+++ notesearch.c
@@ -6,8 +6,9 @@
 #include "error.h"
 #define FILENAME "varnotes"
 void fatal(char *);
-int print_notes(int,int);
+int print_notes(int,int,char*);
 int find_user_notes(int,int);
+int search_filter(char*,char*);
 
 
 
@@ -16,21 +17,29 @@ int main(int argc, char* argv[]){
     int userid, fd,printing;
     printing = 1;
     userid = getuid();
+    char* filter;
+    if(argc > 1){
+        filter = ec_malloc(strlen(argv[1]));
+        strcpy(filter,argv[1]);
+    }
+    else
+        filter = ec_malloc(0);
     printf("opening file\n");
     fd = open(FILENAME,O_RDONLY);
     if(fd == -1)
         fatal("in main() while opening the file\n");
     
-    print_notes(fd, userid);
+    print_notes(fd, userid,filter);
     printf("----------[END]-----------");
     if(close(fd)== -1)
         fatal("while closing the file");
-return 0 ;
+    free(filter);
+    return 0 ;
 }
 
 
 
-int print_notes(int fd, int userid){
+int print_notes(int fd, int userid,char* filter){
     int note_length;
     char * heap;
     note_length = find_user_notes(fd,userid);
@@ -43,6 +52,7 @@ int print_notes(int fd, int userid){
     if(reader == -1)
         fatal("error while storing note in heap");
     heap[note_length]='\0';
+    if(search_filter(heap,filter))
     printf("%s",heap);
     free(heap);
     note_length = find_user_notes(fd,userid);
@@ -70,4 +80,28 @@ int find_user_notes(int fd, int uid){
     lseek(fd,length *-1,SEEK_CUR);
     return length;
 }
+int search_filter(char* note,char*keyword){
+    int note_len = strlen(note);
+    int key_len = strlen(keyword);
+    int i , j;
+    int all_matched=0;
+    if(key_len==0) return 1;
+
+    for(i=0;i<=note_len-key_len;i++){
+        if(note[i]== keyword[0]){
+            all_matched=1;
+                for(j=1;j<key_len;j++){
+                    if(note[i+j]!=keyword[j]){
+                    all_matched=0;
+                    break;}
+                }
+
+        }
+    if(all_matched==1){
+        return 1;}
+    }
+    return 0;
+
+}
+
 
