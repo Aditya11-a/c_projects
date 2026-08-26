b9f6d4569626056c42e3c508a87154da7743ecd5
Aditya11-a <adityaraj5491@gmail.com>
2026-08-26 22:37:46 +0530

    created charactercounter

--- excersice2.c
+++ excersice2.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int c ;
+    int i, c ;
+    int nwhitesp = 0;
+    int notherc = 0 ;
+
+    int ndigit[10];
+    for(i=0;i<10;i++)
+        ndigit[i] = 0 ;
     while((c=getchar()) != EOF){
-        if (c == '\t')
-            printf("\\t");
-        else if (c == '\b')
-            printf("\\b");
-        else if (c == '\\')
-            printf("\\\\");
+        if(c >= '0' && c<= '9')
+            ndigit[c-'0']++;
+        else if(c == '\n' || c=='\t' || c==' ')
+            nwhitesp++;
         else
-            putchar(c);
+            notherc++;
     }
-    return 0;
+    printf("digits count are as follows\n");
+    for(i=0 ; i<10 ; i++)
+        printf("%d ," , ndigit[i]);
+    printf("no of white_spaces and other characters are %d and %d respectively", nwhitesp,notherc);
 }
